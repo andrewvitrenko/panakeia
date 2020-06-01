6dd08adeab8cf1b4b00b33a93a83b86554f8e488
AndrewVitrenko <57104862+AndrewVitrenko@users.noreply.github.com>
2020-06-01 17:46:23 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,63 @@
+$(window).ready(function() {
+    //-------------slider-------------
+    let width = parseFloat($(window).width());
+    let sliderIsLive;
+
+    if (width <= 767) {
+        $('.menu_list').slick({
+            dots: true,
+        });
+        $('.slick-arrow').text('');
+        $('.slick-arrow').css('display', 'none')
+        sliderIsLive = true;
+    }
+
+    $('.burger').removeClass('active')
+    $('.nav_bar').removeClass('visible')
+
+
+
+
+    //-------------resizing blocks---------------
+    let banner = $('.banner');
+    //let newsletter = $('.newsletter');
+    let bottom__image = $('.bottom__image');
+    let header = $('header');
+
+    banner.css('height', banner.outerWidth()/2000*1321);
+    //newsletter.css('height', newsletter.outerWidth()/2000*383);
+    bottom__image.css('height', bottom__image.outerWidth()/2000*383);
+    header.css('height', header.outerWidth()/2000*147);
+
+    $(window).resize(function() {
+        let width = parseFloat($(window).width())
+        banner.css('height', banner.outerWidth()/2000*1321);
+        //newsletter.css('height', newsletter.outerWidth()/2000*383);
+        bottom__image.css('height', bottom__image.outerWidth()/2000*383);
+        header.css('height', header.outerWidth()/2000*147);
+
+        if (width <= 767) {
+            if (!sliderIsLive) {
+                $('.menu_list').slick({
+                    dots: true,
+                });
+                $('.slick-arrow').text('');
+                $('.slick-arrow').css('display', 'none')
+                sliderIsLive = true;
+            }
+        }
+        else if (sliderIsLive && width >= 768) {
+            $('.menu_list').slick('unslick')
+            sliderIsLive = false;
+        }
+    })
+})
+
+//---------------создаем выезд меню------------------------
+
+$('.burger').click(function() {
+    $('.nav_bar').toggleClass('visible')
+    $('.burger').toggleClass('active')
+    $('html').toggleClass('static')
+    $('body').toggleClass('static');
+})
